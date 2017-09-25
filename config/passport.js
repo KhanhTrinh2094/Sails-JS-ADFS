bbf42f4395e5a63e381b41c7a7dab2e87ede7e5a
TRINH.NCK <trinh.nck@gmail.com>
2017-09-26 01:27:51 +0700

    Update passport : serialize user data

--- config/passport.js
+++ config/passport.js
@@ -2,27 +2,24 @@ var passport = require('passport');
 var SamlStrategy = require('passport-saml').Strategy;
 
 passport.serializeUser(function(user, done) {
-  done(null, user.id);
+  done(null, user);
 });
 
-passport.deserializeUser(function(id, done) {
-  User.findOne({ id: id } , function (err, user) {
-    done(err, user);
-  });
+passport.deserializeUser(function(user, done) {
+  done(null, user);
 });
 
 passport.use(new SamlStrategy(
   {
     entryPoint: 'https://myapps.microsoft.com/signin/test-saml2/2898db16-d63f-43eb-bcd0-15c3fc6c443a',
-    issuer: 'https://localhost:1337/callback',
-    callbackUrl: 'https://localhost:1337/callback',
-    thumbprint: '861299E8E897579538AD97F29F4AF344874879E0',
-    identifierFormat: null,
-    signatureAlgorithm: 'sha256'
+    thumbprint: '861299E8E897579538AD97F29F4AF344874879E0'
   },
   function(profile, done) {
-    
-    console.log(profile);
-    done(null);
+    var data = {
+      email: profile['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'],
+      givenname: profile['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname'],
+      surname: profile['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname']
+    }
+    done(null, data);
   })
 );
